4f4ab62a2fb81b6f58b64f71ccb84d453e7fa879
Yusuf KORKMAZ <yusufkorkmazz98@hotmail.com>
2018-09-11 19:57:54 +0300

    dört işlem yapıldı

---
+++ j101e_3/readme.md
@@ -0,0 +1,4 @@
+Dört İşlem
+=================
+
+Tam ve virgüllü iki sayının dört işlem yapıldıktan sonra çıkan sonuçları ekrana yazdırdık.
---
+++ j101e_3/src/j101e_3/dortIslem.java
@@ -0,0 +1,20 @@
+package j101e_3; 
+public class dortIslem { 
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        System.out.println("a + b = "+(a+b));
+        System.out.println("a - b = "+(a-b));
+        System.out.println("a / b = "+(a/b));
+        System.out.println("a * b = "+(a*b)); 
+        
+        System.out.println("");
+        
+        float c = 0.9f;
+        float d = 0.5f;
+        System.out.println("c + d = "+(c+d));
+        System.out.println("c - d = "+(c-d));
+        System.out.println("c / d = "+(c/d));
+        System.out.println("c * d = "+(c*d)); 
+    }   
+}
