aff1c062429694f68d4361102f527c3b80f87778
Yusuf KORKMAZ <yusufkorkmazz98@hotmail.com>
2018-10-02 21:53:31 +0300

    For Döngüsü

---
+++ j101e_6/src/ForDongusu.java
@@ -0,0 +1,22 @@
+public class ForDongusu { 
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i+ " "); 
+        }
+        System.out.println("");
+       
+        for (int i = 10; i > 0; i--) {
+            System.out.print(i+ " ");
+        }
+        System.out.println(""); 
+        
+        int a[]={1,2,3,4,5};
+        for (int elemanlar : a) {
+           System.out.print(elemanlar+ " ");
+        }
+        
+        for ( ; ; ) {
+            //Sonsuz döngüdür. İçine yazılan kodu sonsuza kadar çalıştırır. 
+        }
+    }  
+}
