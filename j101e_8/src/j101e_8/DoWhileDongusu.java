0ce99bbdb888636b7026771c9d378853e3c512ea
Yusuf KORKMAZ <yusufkorkmazz98@hotmail.com>
2018-10-04 18:27:20 +0300

    Do While Döngüsü

---
+++ j101e_8/src/j101e_8/DoWhileDongusu.java
@@ -0,0 +1,11 @@
+package j101e_8; 
+public class DoWhileDongusu {
+    public static void main(String[] args) {
+         int i=10;
+         do{
+            System.out.print(i+ " ");
+            i--;
+         }
+         while(i>0);
+    }
+}
